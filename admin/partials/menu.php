bf8c014c899886779ae2bf5c9e1e2af4880c20a4
Ahrkar kyaw <ahrkarkyaw0@gmail.com>
2022-01-26 18:06:36 +0630

    foodorderbyyoutube

---
+++ admin/partials/menu.php
@@ -0,0 +1,31 @@
+<?php 
+
+    include('../config/constants.php'); 
+    include('login-check.php');
+
+?>
+
+
+<html>
+    <head>
+        <title>Food Order Website - Home Page</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    </head>
+    
+    <body>
+        <!-- Menu Section Starts -->
+        <div class="menu text-center">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="manage-admin.php">Admin</a></li>
+                    <li><a href="manage-category.php">Category</a></li>
+                    <li><a href="manage-food.php">Food</a></li>
+                    <li><a href="manage-order.php">Order</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- Menu Section Ends -->
